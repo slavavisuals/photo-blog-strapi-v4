ec31360329c7d2660289bf21bf272c54e9d5cd7a
slavavisuals <slavavisuals@gmail.com>
2021-12-01 21:43:13 -0500

    change playground settings

--- config/plugins.js
+++ config/plugins.js
@@ -21,7 +21,7 @@ module.exports = ({env}) => ({
     enabled: true,
     config: {
       endpoint: '/graphql',
-      playgroundAlways: true,
+      playground: true,
       defaultLimit: 25,
       maxLimit: 100,
 
